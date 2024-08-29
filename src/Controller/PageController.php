af0a72f143b6e813a314325efee60f6783686ea5
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2024-08-29 16:04:32 +0200

    page, error, contact

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,15 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route; // Am folosit `Annotation\Route` aici pentru claritate
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+        return $this->render('pages/index.html.twig', [
+            'message' => 'Bienvenue',
+        ]);
+    }
+
+    #[Route(
+        path: '/page1/{id}',
+        name: 'page1',
+        requirements: ['id' => '\d+'],
+        defaults: ['id' => 1],
+        methods: ['GET']
+    )]
+    public function article(int $id): Response
+    {
+        return $this->render('pages/page1.html.twig', [
+            'idarticle' => $id,
         ]);
     }
 }
